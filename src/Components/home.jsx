7ef477f1d9891e3b20b3deb79296e12d1ed9d12f
Bill Hu <shhuluojia@163.com>
2023-12-29 12:43:26 +0800

    removed localstorage.getitem(token) in <Home>

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -2,17 +2,17 @@ import TopBar from "./TopBar/TopBar";
 import SideBar from "./SideBar/SideBar";
 import {useNavigate} from "react-router-dom";
 function Home() {
-    const navigate = useNavigate();
-    if (localStorage.getItem("token") === null) {
-        navigate("/login");
-    } else {
+    // const navigate = useNavigate();
+    // if (localStorage.getItem("token") === null) {
+    //     navigate("/login");
+    // } else {
         return (
             <>
                 <TopBar/>
                 <SideBar/>
             </>
         );
-    }
+    // }
 }
 
 export default Home;
