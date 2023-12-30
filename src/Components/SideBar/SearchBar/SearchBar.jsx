677ca3daa127a5c3ef390728a244bdc16b8f58e6
BingnanLi <libn@shanghaitech.edu.cn>
2023-12-30 13:34:10 +0800

    fix bugs of addNewProgram in mobile devices

--- src/Components/SideBar/SearchBar/SearchBar.jsx
+++ src/Components/SideBar/SearchBar/SearchBar.jsx
@@ -4,14 +4,17 @@ import React from "react";
 import "./SearchBar.css"
 
 export default function SearchBar({setSearchedUniv, univList}) {
+    const handleSearch = (event) => {
+        setSearchedUniv(univList.filter((univ) => univ[0].toLowerCase().includes(
+            event.target.value.toLowerCase())));
+    }
+
     return (
-        <form className='SearchBlock' onSubmit={event => {
-            event.preventDefault();
-            setSearchedUniv(univList.filter((univ) => univ[0].toLowerCase().includes(
-                event.target.Search.value.toLowerCase())));
-        }}>
+        <form className='SearchBlock'>
             <FontAwesomeIcon icon={solid("magnifying-glass")}/>
-            <input type='text' id='Search' name='Search' className='Search-bar' placeholder='search for...'/>
+            <input onChange={handleSearch} type='text' id='Search' name='Search'
+                   className='Search-bar' placeholder='search for...'
+            />
         </form>
     )
 }
