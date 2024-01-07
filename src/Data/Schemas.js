cb18ebd58f36c1169877dbda840dbf7a75adeccf
BingnanLi <libn@shanghaitech.edu.cn>
2024-01-07 22:26:39 +0800

    add template for description

--- src/Data/Schemas.js
+++ src/Data/Schemas.js
@@ -1,5 +1,41 @@
-export const DescriptionTemplate = `
-`
+export const DescriptionTemplate = `# \\<项目名称>
+
+## 项目介绍（如果是Master项目）
+
+### 概况
+
+* 项目时长：xx年/xx月
+* 课业压力：
+* 是否需要毕业论文：是/否
+* 是否强制实习：是/否
+* 奖学金情况：有/无
+* 有其他的想法可以继续填。。。
+
+### 详细说明
+
+* 例：120学分项目，毕业通常需要两年半到三年
+* 可以对上面的任何内容做详细阐述
+
+## 项目介绍（如果是PhD项目）
+
+### 概况
+
+* 强Prof还是强Committee？
+* 录取后是不是流转模式
+* 奖学金+stipend情况：xxx
+* 有其他想法可以自己填。。。
+
+### 详细说明
+
+* 可以对上面的任何内容做详细阐述
+
+## 录取偏好/条件
+
+例：偏好有一篇CV顶会一作+强connection
+
+## 其他注意事项
+
+例：网申材料/套磁`
 
 export const ProgramTargetApplicantMajorChoices = ['CS', 'EE', 'IE'];
 export const ProgramRegionChoices = [
