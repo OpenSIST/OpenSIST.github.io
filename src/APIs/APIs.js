2801e6c9f35c8673cc706dcf49ec6ce176f14ad5
BingnanLi <libn@shanghaitech.edu.cn>
2024-02-21 23:12:29 +0800

    change program list api url

--- src/APIs/APIs.js
+++ src/APIs/APIs.js
@@ -1,5 +1,5 @@
 const ROOT = "https://opensist-backend.caoster.workers.dev/";
-export const PROGRAM_LIST = ROOT + "api/static_data/programs";
+export const PROGRAM_LIST = ROOT + "api/list/programs";
 export const PROGRAM_DESC = ROOT + "api/query/program_description";
 export const ADD_MODIFY_PROGRAM = ROOT + "api/mutating/new_modify_program";
 export const SEND_RESET_VERIFY_TOKEN = ROOT + "api/auth/forget";
