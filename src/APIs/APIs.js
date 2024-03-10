c5491318644f5b5eb0c6d6d5693e5c780629dbbf
BingnanLi <libn@shanghaitech.edu.cn>
2024-03-10 21:36:43 +0800

    change the ROOT api

--- src/APIs/APIs.js
+++ src/APIs/APIs.js
@@ -1,4 +1,5 @@
-const ROOT = "https://api.opensist.tech/";
+// const ROOT = "https://api.opensist.tech/";
+const ROOT = "https://demo.opensist.tech/";
 export const PROGRAM_LIST = ROOT + "api/list/programs";
 export const PROGRAM_DESC = ROOT + "api/query/program_description";
 export const ADD_MODIFY_PROGRAM = ROOT + "api/mutating/new_modify_program";
