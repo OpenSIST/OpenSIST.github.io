166df17a3e7c6086495b783eab16f756dcf9a272
boynextdoor <chize@shanghaitech.edu.cn>
2024-03-11 15:42:37 +0800

    remove the redundant get displayName in StatusBlock

--- src/Components/TopBar/StatusBlock/StatusBlock.jsx
+++ src/Components/TopBar/StatusBlock/StatusBlock.jsx
@@ -11,11 +11,9 @@ import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 
 export async function loader() {
-    // console.time("HomeLoader")
     const displayName = await getDisplayName();
-    const metaData = await getMetaData();
-    const avatarUrl = await getAvatar(metaData?.Avatar);
-    // console.timeEnd("HomeLoader")
+    const metaData = await getMetaData(displayName);
+    const avatarUrl = await getAvatar(metaData?.Avatar, displayName);
     return {displayName, avatarUrl};
 }
 
