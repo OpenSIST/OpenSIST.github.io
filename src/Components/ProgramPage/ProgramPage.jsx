7941c59716d3a6ca30b4a1a9c96483fe30e46d83
boynextdoor <chize@shanghaitech.edu.cn>
2024-03-24 17:18:42 +0800

    Rewrite program and profile index page

--- src/Components/ProgramPage/ProgramPage.jsx
+++ src/Components/ProgramPage/ProgramPage.jsx
@@ -4,8 +4,10 @@ import {getPrograms} from "../../Data/ProgramData";
 import './ProgramPage.css';
 import {Paper, useTheme} from "@mui/material";
 import {grey} from "@mui/material/colors";
-import {Add, Edit, Refresh} from "@mui/icons-material";
-import {InlineTypography} from "../common";
+import ReactMarkdown from "react-markdown";
+import MDPath from "../../Data/ProgramIndex.md";
+import {useEffect, useState} from "react";
+import "./ProgramPage.css";
 
 export async function loader({request}) {
     const url = new URL(request.url);
@@ -40,33 +42,15 @@ export default function ProgramPage() {
 }
 
 export function ProgramIndex() {
+    const [markDown, setMarkDown] = useState("");
+    useEffect(() => {
+        fetch(MDPath)
+            .then((response) => response.text())
+            .then((text) => setMarkDown(text));
+    }, []);
     return (
-        <>
-            <h1>项目信息表使用指南</h1>
-            <ul>
-                <li>左方侧边栏中提供搜索框和筛选栏，可快速筛选目标学校或项目。</li>
-                <li>
-                    <InlineTypography>
-                        点击侧边栏中<Add/>按钮可添加新项目。
-                    </InlineTypography>
-                </li>
-                <li>
-                    <InlineTypography>
-                        为缓解后端访问压力，项目信息每10分钟自动于后端同步。用户也可以点击<Refresh/>按钮手动获取最新数据。
-                    </InlineTypography>
-                </li>
-                <li>每个项目卡片中CS/EE等专业名的含义是“哪些专业的学生可申请该项目”。</li>
-                <li>
-                    <InlineTypography>
-                        点开每个项目后，点击右上角处的<Edit/>按钮可修改该项目的信息；点击<Refresh/>手动获取最新数据（每10分钟自动同步）。
-                    </InlineTypography>
-                </li>
-                <li>若对项目列表有任何问题，可以点击侧边栏底部的链接前往
-                    <a href="https://github.com/OpenSIST/OpenSIST.github.io/issues">
-                        GitHub
-                    </a>提issue。
-                </li>
-            </ul>
-        </>
+        <ReactMarkdown className="ProgramIndex">
+            {markDown}
+        </ReactMarkdown>
     )
 }
