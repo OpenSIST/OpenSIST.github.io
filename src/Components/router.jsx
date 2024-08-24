021d8137a1ee223e077e7d46ff5de6f5644b698e
Beiming Zhang <thebeimingzhang@outlook.com>
2024-08-24 15:59:51 +0800

    Fix datapoints program refresh

--- src/Components/router.jsx
+++ src/Components/router.jsx
@@ -112,6 +112,7 @@ const router = createBrowserRouter([
                                         path: '/datapoints/program/:programId',
                                         element: <ProgramContentInDataPoints/>,
                                         loader: programContentLoader,
+                                        action: programContentAction
                                     }
                                 ]
                             }
