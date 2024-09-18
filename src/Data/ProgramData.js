6f3fee7b2e68943d441cb19fa22ce097ef3f1a81
Beiming Zhang <thebeimingzhang@outlook.com>
2024-09-18 19:57:06 +0800

    Fix favorites search

--- src/Data/ProgramData.js
+++ src/Data/ProgramData.js
@@ -61,9 +61,7 @@ export async function getPrograms(isRefresh = false, query = {}) {
 
     const search_programs = {}
     search_keys.forEach((key) => {
-        search_programs[key] = programs[key].filter((programInfo) => {
-            return programInfo.Program.toLowerCase().includes(query.u?.toLowerCase() ?? '');
-        })
+        search_programs[key] = programs[key]
     })
 
 
